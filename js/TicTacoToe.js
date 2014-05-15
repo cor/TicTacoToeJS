b38a5641c57a86e944fa0711e955552bab4373e5
Cor Pruijs <cor@pruys.com>
2014-05-15 21:15:10 +0200

    Did a lot of refactoring, made the victory checking smarter, added tie checking and did some styling

--- js/TicTacoToe.js
+++ js/TicTacoToe.js
@@ -3,45 +3,70 @@ var currentTurn = 1;
 var gameRunning = true;
 var gameWinner = 0;
 
-function clearLevel () {
-	for (var i = level.length - 1; i >= 0; i--) {
-		level[i] = 0;
-	}
-}
+$(document).ready(function(){
+	clearLevel();
+	updateLevel();
 
-function checkForVictory (playerToCheck) {
-	//horizontal
-	if (level[0] == playerToCheck && level[1] == playerToCheck && level[2] == playerToCheck) return true;
-	if (level[3] == playerToCheck && level[4] == playerToCheck && level[5] == playerToCheck) return true;
-	if (level[6] == playerToCheck && level[7] == playerToCheck && level[8] == playerToCheck) return true;
-	//vertical
-	if (level[0] == playerToCheck && level[3] == playerToCheck && level[6] == playerToCheck) return true;
-	if (level[1] == playerToCheck && level[4] == playerToCheck && level[7] == playerToCheck) return true;
-	if (level[2] == playerToCheck && level[5] == playerToCheck && level[8] == playerToCheck) return true;
-	//diagonal
-	if (level[0] == playerToCheck && level[4] == playerToCheck && level[8] == playerToCheck) return true;
-	if (level[2] == playerToCheck && level[4] == playerToCheck && level[6] == playerToCheck) return true;
-	//none
-	return false;
-}
+		$( ".lf0" ).click(function() {
+			if (gameRunning) setLevelField(0, currentTurn);
+			else resetGame();
+		});
 
-function aiTurn () {
+		$( ".lf0" ).click(function() {
+			if (gameRunning) setLevelField(0, currentTurn);
+			else resetGame();
+		});
 
-	for (var i = level.length - 1; i >= 0; i--) {
-		level[i]
-	};
-}
 
-function setLevelField(levelField, value) {
-	if (level[levelField] === 0) {
-		level[levelField] = value;
-		changeTurn();
-	}
-}
+		$( ".lf1" ).click(function() {
+			if (gameRunning) setLevelField(1, currentTurn);
+			else resetGame();
+		});
+
+		$( ".lf2" ).click(function() {
+			if (gameRunning) setLevelField(2, currentTurn);
+			else resetGame();
+		});
+
+		$( ".lf3" ).click(function() {
+			if (gameRunning) setLevelField(3, currentTurn);
+			else resetGame();
+
+		});
+
+		$( ".lf4" ).click(function() {
+			if (gameRunning) setLevelField(4, currentTurn);
+			else resetGame();
+
+		});
+
+		$( ".lf5" ).click(function() {
+			if (gameRunning) setLevelField(5, currentTurn);
+			else resetGame();
+		});
+
+		$( ".lf6" ).click(function() {
+			if (gameRunning) setLevelField(6, currentTurn);
+			else resetGame();
+		});
+
+		$( ".lf7" ).click(function() {
+			if (gameRunning) setLevelField(7, currentTurn);
+			else resetGame();
+		});
+
+		$( ".lf8" ).click(function() {
+			if (gameRunning) setLevelField(8, currentTurn);
+			else resetGame();
+		});
+
+		$( ".levelField").click(function() {
+			updateLevel();
+		});
+});
 
 function updateLevel () {
 	for (var i = level.length - 1; i >= 0; i--) {
-		
 		if(level[i] === 0) {
 			$(".lf" + i).css("background-color", "#a4a4a4");
 		} 
@@ -58,106 +83,63 @@ function updateLevel () {
 	if (checkForVictory(1) == true) {
 		gameRunning = false;
 		gameWinner = 1;
-		showVictory();
 	}
 	
 	else if (checkForVictory(2) == true) {
 		gameRunning = false;
 		gameWinner = 2;
-		showVictory();
+	}
+	if (gameIsTie()) {
+		gameRunning = false;
+		gameWinner = 4;
+	}
+
+	if(gameWinner == 0 ) $(".victoryDisplay").html('<p><p>');
+	if(gameWinner == 1 ) $(".victoryDisplay").html('<p class="blueVictory">Blue has won!</p>');
+	if(gameWinner == 2 ) $(".victoryDisplay").html('<p class="redVictory">Red has won!</p>');
+	if(gameWinner == 4 ) $(".victoryDisplay").html('<p class="tieVictory">The game is a Tie</p>');
+}
+
+function checkForVictory (playerToCheck) {
+	var winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+	for (var i = winningMoves.length - 1; i >= 0; i--) {
+		if (level[winningMoves[i][0]] == playerToCheck && level[winningMoves[i][1]] == playerToCheck && level[winningMoves[i][2]] == playerToCheck ) return true;
+	}
+	return false;
+}
+
+function gameIsTie() {
+	var gameIsTieVar = true;
+	for (var i = level.length - 1; i >= 0; i--) {
+		if (level[i] == 0) gameIsTieVar = false;
+	}
+	return gameIsTieVar;
+}
+
+function setLevelField(levelField, value) {
+	if (level[levelField] === 0) {
+		level[levelField] = value;
+		changeTurn();
 	}
 }
 
+function resetGame (){
+	gameRunning = true;
+	gameWinner = 0;
+	clearLevel();
+	updateLevel();
+}
+
+function clearLevel () {
+	for (var i = level.length - 1; i >= 0; i--) {
+		level[i] = 0;
+	}
+}
 function changeTurn () {
 	if (currentTurn == 1) {
 		currentTurn = 2;
-	}
-
-	else if (currentTurn == 2) {
+	} else if (currentTurn == 2) {
 		currentTurn = 1;
-	}
-	else { currentTurn = -1; }
+	} else { currentTurn = -1; }
 }
-
-function showVictory () {
-	if(gameWinner == 1 ) $(".victoryDisplay").html('<p class="blueVictory">Blue has won!</p>')
-	if(gameWinner == 2 ) $(".victoryDisplay").html('<p class="redVictory">Red has won!</p>')
-}
-
-$(document).ready(function(){
-	clearLevel();
-	updateLevel();
-
-		$( ".lf0" ).click(function() {
-			if (gameRunning) {
-			setLevelField(0, currentTurn);
-			updateLevel();
-			} 
-		});
-
-		$( ".lf0" ).click(function() {
-			if (gameRunning) {
-			setLevelField(0, currentTurn);
-			updateLevel();
-			} 
-		});
-
-
-		$( ".lf1" ).click(function() {
-			if (gameRunning) {
-			setLevelField(1, currentTurn);
-			updateLevel();
-			} 
-		});
-
-		$( ".lf2" ).click(function() {
-			if (gameRunning) {
-			setLevelField(2, currentTurn);
-			updateLevel();
-			}
-		});
-
-		$( ".lf3" ).click(function() {
-			if (gameRunning) {
-			setLevelField(3, currentTurn);
-			updateLevel();
-			} 
-
-		});
-
-		$( ".lf4" ).click(function() {
-			if (gameRunning) {
-			setLevelField(4, currentTurn);
-			updateLevel();
-			} 
-
-		});
-
-		$( ".lf5" ).click(function() {
-			if (gameRunning) {
-			setLevelField(5, currentTurn);
-			updateLevel();
-			}	 
-		});
-
-		$( ".lf6" ).click(function() {
-			if (gameRunning) {
-			setLevelField(6, currentTurn);
-			updateLevel();
-			} 
-		});
-
-		$( ".lf7" ).click(function() {
-			if (gameRunning) {
-			setLevelField(7, currentTurn);
-			updateLevel();
-			} 
-		});
-
-		$( ".lf8" ).click(function() {
-			if(gameRunning) {
-			setLevelField(8, currentTurn);
-	 		updateLevel();
-			}
-		});
-});
